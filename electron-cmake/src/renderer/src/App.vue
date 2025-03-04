adceff06b9e7cdffb763f19781cbd72dde99616e
lorilu-studio <1229166932@qq.com>
2025-03-04 14:57:31 +0800

    feat: cmake build electron cpp module

---
+++ electron-cmake/src/renderer/src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+// import Versions from './components/Versions.vue'
+
+// const ipcHandle = () => window.electron.ipcRenderer.send('ping')
+import { ref } from 'vue'
+
+const num1 = ref(0)
+const num2 = ref(0)
+const count = ref(0)
+
+const handleAdd = async () => {
+  const result = await window.electron.ipcRenderer.invoke('add-fun', num1.value, num2.value)
+  count.value = result
+}
+</script>
+
+<template>
+  <div>
+    <input v-model="num1" type="text" /> + <input v-model="num2" type="text" />
+    <button @click="handleAdd">计算</button>
+
+    <div>{{ count }}</div>
+  </div>
+  <!-- <div class="actions">
+    <div class="action">
+      <a target="_blank" rel="noreferrer" @click="ipcHandle">Send IPC</a>
+    </div>
+  </div> -->
+  <!-- <Versions /> -->
+
+</template>
